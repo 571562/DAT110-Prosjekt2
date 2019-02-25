2499652d0d79ea808f13d65855ae92eef6098e6d
571562 <herborgisjo@hotmail.com>
2019-02-25 13:48:04 +0100

    Task 3 done

--- dat110-project2-startcode-master/project2/src/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ dat110-project2-startcode-master/project2/src/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -6,8 +6,8 @@ import no.hvl.dat110.client.Client;
  * The type Temperature device.
  */
 public class TemperatureDevice {
-	
-	private static final int COUNT = 10;
+
+    private static final int COUNT = 10;
 
     /**
      * The entry point of application.
@@ -15,16 +15,29 @@ public class TemperatureDevice {
      * @param args the input arguments
      */
     public static void main(String[] args) {
-		
-		TemperatureSensor sn = new TemperatureSensor();
-		
-		// TODO - start
-		
-		// TODO - end
-		
-		System.out.println("Temperature device stopping ... ");
-		
-		throw new RuntimeException("not yet implemented");
-		
-	}
+
+        TemperatureSensor sn = new TemperatureSensor();
+
+        // TODO - start
+        Client tempClient = new Client("tempDevice", Common.BROKERHOST, Common.BROKERPORT);
+
+        tempClient.connect();
+
+        for (int i = 0; i < COUNT; i++) {
+            tempClient.publish(Common.TEMPTOPIC, String.valueOf(sn.read()));
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        tempClient.disconnect();
+        // TODO - end
+
+        System.out.println("Temperature device stopping ... ");
+
+
+    }
 }
